e0ae21eeb999e856dbbd644eafda6d32fc05f69c
Amrutha010 <karrinagadurgaamrutha040@gmail.com>
2023-03-28 13:03:13 +0530

    folder structure created

---
+++ TravelEase/src/main/java/com/travelease/TravelEaseApplication.java
@@ -0,0 +1,13 @@
+package com.travelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelEaseApplication.class, args);
+	}
+
+}
---
+++ TravelEase/src/test/java/com/travelease/TravelEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
