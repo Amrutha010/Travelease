e50005864769d5d2dba6cac0e536d64ca35f8b0e
Syed Sidra <guest1@ZEBRAs-MacBook-Air.local>
2023-03-30 05:14:48 +0530

    partially worked on hotelimpl login sessions

--- TravelEase/src/main/java/com/travelease/service/HotelService.java
+++ TravelEase/src/main/java/com/travelease/service/HotelService.java
@@ -3,17 +3,18 @@ package com.travelease.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.travelease.Exceptions.HotelException;
 import com.travelease.models.Hotel;
 
 public interface HotelService {
 	
 	public Hotel createHotel(Hotel hotel);
 
-	public Optional<Hotel> getHotelById(Integer id);
+	public Hotel getHotelById(Integer id)throws HotelException ;
 	
-	public Optional<Hotel> deleteHotel(Integer id);
+	public Optional<Hotel> deleteHotel(Integer id)throws HotelException;
 
-	public List<Hotel> AllHotels();
+	public List<Hotel> AllHotels()throws HotelException;
 
 	
 	
---
+++ TravelEase/src/main/java/com/travelease/service/LogInServiceImpl.java
@@ -0,0 +1,21 @@
+package com.travelease.service;
+
+import com.travelease.Exceptions.LoginException;
+import com.travelease.models.LogIn;
+import com.travelease.models.UserType;
+
+public class LogInServiceImpl implements LogInService {
+
+	@Override
+	public String logIntoAccount(LogIn login) throws LoginException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String logOutFromAccount(Integer userId, UserType userType) throws LoginException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
