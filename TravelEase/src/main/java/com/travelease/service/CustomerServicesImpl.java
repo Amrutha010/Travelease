a517b5f602e2c9b0f5d5f32a41af923606c8a13e
Vikas Sanodiya <103686747+vikas-011@users.noreply.github.com>
2023-04-01 01:09:12 +0530

    Costumer,feedback

---
+++ TravelEase/src/main/java/com/travelease/service/CustomerServicesImpl.java
@@ -0,0 +1,82 @@
+package com.travelease.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.travelease.Exceptions.CustomerException;
+import com.travelease.models.Customer;
+import com.travelease.repository.CustomerDAO;
+
+@Service
+public class CustomerServicesImpl implements CustomerServices{
+	
+	@Autowired
+	private CustomerDAO cDao;
+
+	@Override
+	public Customer addCustomer(Customer customer) throws CustomerException {
+		
+		return cDao.save(customer);
+		
+	}
+
+	@Override
+	public Customer updateCustomer(Customer customer) throws CustomerException {
+		cDao.findById(customer.getCustomerId()).orElseThrow(()-> new CustomerException("Customer does not exist with Id:- "+customer.getCustomerId()));
+		
+		return cDao.save(customer);
+	}
+
+	@Override
+	public Customer deleteCustomer(Integer customerId) throws CustomerException {
+
+		Optional<Customer> opt = cDao.findById(customerId);
+		
+		if(opt.isPresent()) {
+			Customer customer = opt.get();
+			cDao.delete(customer);
+			return customer;
+		}else {
+			throw new CustomerException("Customer does not exist with Id:- "+customerId);
+		}
+		
+	}
+	
+	
+
+	@Override
+	public Customer viewCustomer(Integer customerId) throws CustomerException {
+		
+		return cDao
+				.findById(customerId)
+				.orElseThrow(() -> 
+				new CustomerException("No Customer found with Id:- "+customerId));
+		
+	}
+	
+
+
+	@Override
+	public List<Customer> viewAllCustomer() throws CustomerException {
+
+		List<Customer> customers = cDao.findAll();
+		
+		if (customers.size()!=0) {
+			
+			return customers;
+			
+		}else {
+			
+			throw new CustomerException("No Customer found!");
+			
+		}
+		
+	}
+	
+
+	
+
+}
---
+++ TravelEase/src/main/java/com/travelease/service/FeedbackServicesImpl.java
@@ -0,0 +1,100 @@
+package com.travelease.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.travelease.Exceptions.CustomerException;
+import com.travelease.Exceptions.FeedbackException;
+import com.travelease.models.Feedback;
+import com.travelease.models.FeedbackDTO;
+import com.travelease.models.Packages;
+import com.travelease.repository.FeedbackDAO;
+
+public class FeedbackServicesImpl implements FeedbackServices{
+
+	@Autowired
+	private FeedbackDAO fDao;
+	
+	@Autowired
+	private BookingServices bServices;
+	
+	@Autowired
+	private CustomerServices cServices;
+	
+	@Autowired
+	private PackageServices pServices;
+
+	@Override
+	public Feedback addFeedback(FeedbackDTO feedback) throws FeedbackException, BookingException, PackageException { //use feedbackDto
+		
+		Feedback newFeedback= new Feedback();
+		newFeedback.setFeedback(feedback.getFeedback());
+		newFeedback.setRating(feedback.getRating());
+		newFeedback.setBooking(bServices.ViewBookingById(feedback.getBookingId()));
+		newFeedback.setCustomer(bServices.ViewBookingById(feedback.getBookingId()).getCustomer());
+		Packages p= pServices.searchPackageById(feedback.getPackageId());
+		
+		newFeedback.setPackages(p);
+		
+		Feedback feedback2= fDao.save(newFeedback);
+		
+		List<Feedback> feedbacks= findFeedbackByPackageId(p.getPackageId());
+		double sum=0;
+		for (Feedback feedback21 : feedbacks) {
+			sum+=feedback21.getRating();
+		}
+		double avg =sum/feedbacks.size();
+		
+		p.setPackageRating(avg+"");
+		
+		pServices.updatePackage(p);
+		
+		return feedback2;
+		
+//		fDao.getavgfeedbackbypackId() //2
+		//update average rating inside package 3
+//		packageService.updatePackage.save(package) //4
+	}
+
+	@Override
+	public Feedback findFeedbackByFeedbackId(Integer feedbackId) throws FeedbackException {
+
+		return fDao.findById(feedbackId)
+				.orElseThrow(()-> 
+				new FeedbackException("No feedback found with feedback Id:- "+feedbackId));
+		
+	}
+
+	@Override
+	public List<Feedback> findFeedbackByCustomerId(Integer customerId)throws CustomerException {
+		
+		return cServices.viewCustomer(customerId).getFeedbacks();
+		
+		
+	}
+	
+	@Override
+	public List<Feedback> findFeedbackByPackageId(Integer packageId) throws PackageException{
+		
+		Packages mypackage=pServices.searchPackageById(packageId);
+		
+		return fDao.findByPackages(mypackage);
+		
+	}
+	
+	
+
+	@Override
+	public List<Feedback> viewAllFeedback() throws FeedbackException {
+		List <Feedback> feedbacks = fDao.findAll();
+		
+		if (feedbacks.size()==0) {
+			throw new FeedbackException("No Feedbacks present");
+		} else {
+			return feedbacks;
+		}
+		
+	}
+
+}
